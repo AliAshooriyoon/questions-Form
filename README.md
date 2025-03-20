fe0e104c9349d0b23e955755cce24b0dd6122e91
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-03-20 12:21:45 +0330

    Base Files

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a Questions Project with ReactJS
---
+++ src/components/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import "./Home.css";
+const Home = () => {
+  return (
+    <>
+      <div className="home">Haus</div>
+    </>
+  );
+};
+export default Home;
