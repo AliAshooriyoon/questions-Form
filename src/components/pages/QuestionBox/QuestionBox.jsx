4d234a90e4bed86a0cfc37cfc3691d54e5f894f5
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-03-21 14:12:24 +0330

    setTimer

--- src/components/pages/QuestionBox/QuestionBox.jsx
+++ src/components/pages/QuestionBox/QuestionBox.jsx
@@ -1,18 +1,46 @@
 import { useState } from "react";
 import "./QuestionBox.css";
 import { questions } from "../../questions";
+import { useEffect } from "react";
+import { useRef } from "react";
 const QuestionBox = () => {
+  //disable-eslint-next-line
   const [allQuestions, setAllQuestions] = useState(questions);
-  const [counter, setCounter] = useState(0);
+  //disable-eslint-next-line
+  const [counter, setCounter] = useState(1);
+  const [timer, setTimer] = useState(5);
+  useEffect(() => {
+    const changeTimer = setInterval(() => {
+      console.log(timer);
+      setTimer((time) => (time > 0 ? time - 1 : 0));
+    }, 1000);
+    return () => {
+      clearInterval(changeTimer);
+    };
+  });
   return (
     <>
       <div className="questionBox  p-6 rounded-2xl w-[70%]">
+        <div className="timer w-[60%]  border-4 h-10 mx-auto mb-6 rounded-2xl">
+          {/* {timer} {timer > 1 ? "Sekunden" : "Sekunde"} */}
+          <div
+            className={`colorFiler bg-indigo-800 h-full rounded-2xl text-center`}
+            style={{ width: `${timer * 20}%` }}
+          ></div>
+        </div>
         <div className="questionBox__titleBox flex flex-wrap gap-6">
           <h2 className="titleBox__title text-center mb-2 mx-auto">
             {allQuestions[counter].title}
           </h2>
           {allQuestions[counter].options.map((i) => {
-            return <div className="questionBox__itemsBox__item">{i}</div>;
+            return (
+              <div
+                className="questionBox__itemsBox__item"
+                onClick={() => setCounter(counter + 1)}
+              >
+                {i}
+              </div>
+            );
           })}
         </div>
       </div>
--- src/components/questions.js
+++ src/components/questions.js
@@ -8,6 +8,7 @@ export const questions = [
       "Die ist ein Beispiel für  ist. Es gibt noch viele weitere Arten des Hauses,",
       "Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist",
     ],
+    answer: 2,
   },
   {
     title:
@@ -15,29 +16,32 @@ export const questions = [
     id: 2,
     options: [
       "Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses,",
-      " auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses,",
-      "Die ist ein Beispiel für  ist. Es gibt noch viele weitere Arten des Hauses,",
-      "Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist",
+      " auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses, 2",
+      "Die ist ein Beispiel für  ist. Es gibt noch viele weitere Arten des Hauses,2",
+      "Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist  2",
     ],
+    answer: 3,
   },
   {
     title: "Wie kann man IP Adresse sehen ?",
     id: 3,
     options: [
       "Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses,",
-      " auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses,",
-      "Die ist ein Beispiel für  ist. Es gibt noch viele weitere Arten des Hauses,",
-      "Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist",
+      " auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses, 3",
+      "Die ist ein Beispiel für  ist. Es gibt noch viele weitere Arten des Hauses,3",
+      "Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist  3",
     ],
+    answer: 4,
   },
   {
     title: "Woher kann man externalHards sehen?",
     id: 4,
     options: [
       "Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses,",
-      " auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses,",
-      "Die ist ein Beispiel für  ist. Es gibt noch viele weitere Arten des Hauses,",
-      "Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist",
+      " auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses, 4",
+      "Die ist ein Beispiel für  ist. Es gibt noch viele weitere Arten des Hauses,4",
+      "Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist  4",
     ],
+    answer: 1,
   },
 ];
