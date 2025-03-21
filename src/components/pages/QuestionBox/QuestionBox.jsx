0161f6e4f29e3943df59cd4fe5151fe5b9112a89
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-03-21 14:22:54 +0330

    set EndLevel for Questions

--- src/components/pages/QuestionBox/QuestionBox.jsx
+++ src/components/pages/QuestionBox/QuestionBox.jsx
@@ -10,6 +10,7 @@ const QuestionBox = () => {
   const [counter, setCounter] = useState(1);
   const [timer, setTimer] = useState(5);
   useEffect(() => {
+    setTimer(5);
     const changeTimer = setInterval(() => {
       console.log(timer);
       setTimer((time) => (time > 0 ? time - 1 : 0));
@@ -17,33 +18,40 @@ const QuestionBox = () => {
     return () => {
       clearInterval(changeTimer);
     };
-  });
+  }, [counter]);
   return (
     <>
-      <div className="questionBox  p-6 rounded-2xl w-[70%]">
-        <div className="timer w-[60%]  border-4 h-10 mx-auto mb-6 rounded-2xl">
-          {/* {timer} {timer > 1 ? "Sekunden" : "Sekunde"} */}
-          <div
-            className={`colorFiler bg-indigo-800 h-full rounded-2xl text-center`}
-            style={{ width: `${timer * 20}%` }}
-          ></div>
+      {counter ? (
+        <div className="questionBox  p-6 rounded-2xl w-[70%]">
+          <div className="timer w-[60%]  border-4 h-10 mx-auto mb-6 rounded-2xl">
+            {/* {timer} {timer > 1 ? "Sekunden" : "Sekunde"} */}
+            <div
+              className={`colorFiler bg-indigo-800 h-full rounded-2xl text-center`}
+              style={{ width: `${timer * 20}%` }}
+            ></div>
+          </div>
+          <div className="questionBox__titleBox flex flex-wrap gap-6">
+            <h2 className="titleBox__title text-center mb-2 mx-auto">
+              {counter <= allQuestions.length &&
+                allQuestions[counter - 1].title}
+            </h2>
+            {allQuestions[counter - 1].options.map((i) => {
+              return (
+                <div
+                  className="questionBox__itemsBox__item"
+                  onClick={() =>
+                    setCounter((c) => (c < allQuestions.length ? c + 1 : 0))
+                  }
+                >
+                  {i}
+                </div>
+              );
+            })}
+          </div>
         </div>
-        <div className="questionBox__titleBox flex flex-wrap gap-6">
-          <h2 className="titleBox__title text-center mb-2 mx-auto">
-            {allQuestions[counter].title}
-          </h2>
-          {allQuestions[counter].options.map((i) => {
-            return (
-              <div
-                className="questionBox__itemsBox__item"
-                onClick={() => setCounter(counter + 1)}
-              >
-                {i}
-              </div>
-            );
-          })}
-        </div>
-      </div>
+      ) : (
+        <p>End</p>
+      )}
     </>
   );
 };
